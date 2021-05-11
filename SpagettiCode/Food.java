82aea6a312e734afad21bfcab01153223e217adb
Enes Güler <kriptoenes@gmail.com>
2021-05-11 16:11:50 +0300

    Add files via upload

---
+++ SpagettiCode/Food.java
@@ -0,0 +1,16 @@
+package ybu.datastruct;
+
+import java.util.ArrayList;
+
+public class Food {
+    public String name;
+    public ArrayList<Item> items;
+    public String recipe;
+
+    public Food(String name, ArrayList<Item> items, String recipe) {
+        this.name = name;
+        this.items = items;
+        this.recipe = recipe;
+    }
+
+}
---
+++ SpagettiCode/Item.java
@@ -0,0 +1,19 @@
+package ybu.datastruct;
+
+public class Item {
+    public String name;
+    public String type;
+    public String unit;
+    public double amount;
+    public double calories;
+
+    public Item(String name, String type, String unit, double amount, double calories) {
+        this.name = name;
+        this.type = type;
+        this.unit = unit;
+        this.amount = amount;
+        this.calories = calories;
+    }
+
+
+}
